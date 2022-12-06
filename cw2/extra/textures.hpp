ea5f00e13481a02a1ec41129f981b092f43dfc2e
Michael Wright <sc19mw@leeds.ac.uk>
2022-12-06 19:00:54 +0000

    added some more stuff

--- cw2/extra/textures.hpp
+++ cw2/extra/textures.hpp
@@ -2,25 +2,33 @@
 #include "stb_image.h"
 #include <iostream>
 
+
 GLuint createTexture()
 {
     GLuint textureID = 0;
     glGenTextures( 1, &textureID );
     glBindTexture( GL_TEXTURE_2D, textureID );
 
-    /*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int w, h, chan;
-    //unsigned char *idata = stbi_load( "../../texture/markus.png", &w, &h, &chan, 0);
+    unsigned char *idata = stbi_load( "markus.png", &w, &h, &chan, 0);
+    if (idata)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, idata);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    //free(idata);
 
-    //glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, idata);
-    stbi_image_free(idata);
-
-    //glGenerateMipmapes( GL_TEXTURE_2D );*/
+    //glGenerateMipmapes( GL_TEXTURE_2D );
 
     return textureID;
 }
