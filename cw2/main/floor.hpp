0cef819cb81a4fc495de6d825d18c0fd100aab7f
Luke McDowell <sc19ldbm@leeds.ac.uk>
2022-12-10 09:37:04 +0000

    floor

---
+++ cw2/main/floor.hpp
@@ -0,0 +1,10 @@
+constexpr float const kFloorPositions[] = {
+	// coords				normals
+	-1.f, 0.f, -1.f,        0.f, 1.f, 0.f,
+    -1.f, 0.f, 1.f,         0.f, 1.f, 0.f,
+    1.f, 0.f, -1.f,         0.f, 1.f, 0.f,
+
+    1.f, 0.f, -1.f,         0.f, 1.f, 0.f,
+    -1.f, 0.f, 1.f,         0.f, 1.f, 0.f,
+    1.f, 0.f, 1.f,          0.f, 1.f, 0.f,
+};
