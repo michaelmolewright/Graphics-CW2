c80ac7f20fc4879954a9d70ebbf30783e16ebd4e
Luke McDowell <lukemcdowell2014@gmail.com>
2022-12-21 14:58:47 +0000

    workgin texture coords

--- cw2/main/loadobj.cpp
+++ cw2/main/loadobj.cpp
@@ -24,11 +24,27 @@ SimpleMeshData load_wavefront_obj( char const *aPath ) {
         for ( std::size_t i = 0; i < shape.mesh.indices.size(); ++i ) {
             auto const &idx = shape.mesh.indices[i];
 
+            // positions
             ret.positions.emplace_back( Vec3f{
                 result.attributes.positions[idx.position_index * 3 + 0],
                 result.attributes.positions[idx.position_index * 3 + 1],
                 result.attributes.positions[idx.position_index * 3 + 2] } );
 
+            // normals
+            // normal_index or position_index
+            ret.normals.emplace_back( Vec3f{ 
+                result.attributes.normals[idx.normal_index * 3 + 0],
+                result.attributes.normals[idx.normal_index * 3 + 1],
+                result.attributes.normals[idx.normal_index * 3 + 2]
+            });
+
+            // textures
+            ret.textures.emplace_back( Vec2f{
+                result.attributes.texcoords[idx.texcoord_index * 2 + 0],
+                1- result.attributes.texcoords[idx.texcoord_index * 2 + 1]
+            });
+
+            // COLOURS CODE FROM EX4
             // Always triangles, so we can find the face index by dividing the
             // vertex index by three
             // auto const &mat = result.materials[shape.mesh.material_ids[i / 3]];
@@ -38,42 +54,6 @@ SimpleMeshData load_wavefront_obj( char const *aPath ) {
             //     Vec3f{ mat.ambient[0], mat.ambient[1], mat.ambient[2] } );
         }
     }
-
-    // add texture coordinates to SimpleMeshData
-    for ( auto const &shape : result.shapes ) {
-        for ( std::size_t i = 0; i < shape.mesh.indices.size(); ++i ) {
-
-            auto const &idx = shape.mesh.indices[i];
-
-            Vec2f uv{ 
-                result.attributes.texcoords[idx.texcoord_index * 2 ], 
-                result.attributes.texcoords[idx.texcoord_index * 2 +1 ] };
-
-            ret.textures.emplace_back( uv );
-        }
-    }
-
-    // add normals to SimpleMeshData
-    for ( auto const &shape : result.shapes ) {
-        for ( std::size_t i = 0; i < shape.mesh.indices.size(); ++i ) {
-
-            auto const &idx = shape.mesh.indices[i];
-
-            Vec3f norm{ 
-                - result.attributes.normals[idx.normal_index*3 + 0 ], 
-                - result.attributes.normals[idx.normal_index*3 + 1 ],
-                - result.attributes.normals[idx.normal_index*3 + 2 ] };
-
-            ret.normals.emplace_back( norm );
-        }
-    }
-
-    // for ( std::size_t i = 0; i < result.attributes.texcoords.size(); i += 2 ) {
-
-    //         Vec2f uv{ result.attributes.texcoords[i], result.attributes.texcoords[i+1] };
-    //         ret.textures.emplace_back( uv );
-    //     }
-
 
     return ret;
 }
